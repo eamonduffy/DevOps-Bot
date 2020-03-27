3a8c22ef0fb196a4f20bd4f60701756fb50a916d
Eamon Duffy <eamon.duffy0@gmail.com>
2020-03-27 18:01:22 -0400

    create base working bot

---
+++ commands.js
@@ -0,0 +1,12 @@
+// use this file to for the first command functions (the below are just examples so you understand how to export functions that can be used in another javascript file)
+// we will use thes functions in the main.js where the bot will call them
+
+module.exports = {
+  cmd1: () => {
+    return null
+  },
+
+  cmd2: () => {
+    return null
+  }
+}
--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 // main file - this will be the base of the project
-// change 2
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const auth = require('./auth.json');
+
+/// Command Functions ///
+const {
+  cmd1,
+  cmd2
+} = require('./commands')
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login(auth.token);
