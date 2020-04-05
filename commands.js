1478fe90cf745ed0dbefbc5745af4da6967d182e
Mannarinoo <joey_mannarino@yahoo.com>
2020-04-04 23:13:15 -0400

    help command with quotes

--- commands.js
+++ commands.js
@@ -48,7 +48,7 @@ module.exports = {
   }else if(message.content.startsWith('/')) {
 	const tech = message.content.substr('/'.length).toLocaleLowerCase()
 	if(tech.localeCompare('help')) {
-      message.channel.send('"dt continuous integration" - List of technologies used in continuous integration. \n"dt continuous testing" - List of technologies used in continuous testing. \n"dt source control practices" - List of technologies used in source control. \n"dt continuous monitoring" - List of technologies used in continuous monitoring. \n"dt continuous exploration" - List of technologies used in continuous exploration. \n"dt chatops" - List of technologies used in chatops. \n"dt package management" - List of technologies used in package management. \n"dt devsecops" - List of technologies used in devsecops. ')
+      message.channel.send('"dt continuous integration" - List of technologies used in continuous integration. \n"dt continuous testing" - List of technologies used in continuous testing. \n"dt source control practices" - List of technologies used in source control. \n"dt continuous monitoring" - List of technologies used in continuous monitoring. \n"dt continuous exploration" - List of technologies used in continuous exploration. \n"dt chatops" - List of technologies used in chatops. \n"dt package management" - List of technologies used in package management. \n"dt devsecops" - List of technologies used in devsecops.\n"quote" - displays informative DevOps quotes ')
 	  
     }else{
       message.channel.send('Please enter a valid command and try again :)')
